22e2208a8ef463f1941380c249276cf5170a5121
Alexander Winzig <alex.winzig@gmx.de>
2021-08-18 22:46:49 +0200

    Added Manifest.webmanifest to head

--- serviceworker.js
+++ serviceworker.js
@@ -1,8 +1,8 @@
-/*
-const CACHE_NAME = "offline-v2";
+
+const CACHE_NAME = "offline-v3";
 const ASSETS = [
     "/",
-    "/Datenbank.html",
+    "/Wizard/Datenbank.html",
     "/Fallbackpage.html",
     "/Funktionsweise.html",
     "/index.html",
@@ -14,15 +14,48 @@ const ASSETS = [
     "/pwa3.html",
     "/ServiceWorker.html",
     "/WebAPI.html",
+    "/Cache-Strategie.html",
+    "/Dynamisches-Cachen.html",
     "/manifest.webmanifest",
     "/app.js",
     "/styles.css",
-    "/Cache-Strategie.html",
-    "/Dynamisches-Cachen.html",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css",
+    "https://code.jquery.com/jquery-3.3.1.slim.min.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js",
+    "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js",
     "/IndexedDB.html",
     "/img/icon192.png",
     "/img/icon512.png",
     "/img/PWA_Logo.png",
+    "/img/AddPerson.png",
+    "/img/Cache-Fallbackpage.png",
+    "/img/Cache-web-dev-strategie.png",
+    "/img/Cache_Fallback.png",
+    "/img/Cache_on_Interaction.png",
+    "/img/Cache_on_PushMessage.png",
+    "/img/Cache_only.png",
+    "/img/Cache_then_Network.png",
+    "/img/Chrome.JPG",
+    "/img/Database_PWA_0.png",
+    "/img/Database_PWA_1.png",
+    "/img/Installieren.png",
+    "/img/Installieren_Smartphone.JPG",
+    "/img/Installiert.JPG",
+    "/img/Installiert_vs_Chrome.JPG",
+    "/img/Lighthouse.png",
+    "/img/PC_Vergleich_PWA_Chrome.png",
+    "/img/Personentabelle.png",
+    "/img/PWA1-Bild.png",
+    "/img/PWA2_DynamicCache.png",
+    "/img/PWA2_StaticCache.png",
+    "/img/PWA_Uebersicht.png",
+    "/img/Read-Only_PWA_Bild.png",
+    "/img/service-worker-infografik.jpg",
+    "/img/SW.jpeg",
+    "/img/SWPruefen.png",
+    "/img/WAM_Chrome.png",
+
+
     //Alle anderen Bilder
 ];
 
@@ -53,5 +86,5 @@ self.addEventListener("fetch", event => {
         })
     )
 });
-*/
+
 
